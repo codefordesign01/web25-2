f51e73b02f48cea1c1d5a0c426cf7fa1a588a096
shahzad <shahzadmughals278@gmail.com>
2025-11-25 16:24:30 +0500

    java function assignment

---
+++ main.js
@@ -0,0 +1,19 @@
+var a = "10";
+           var students = [
+           	{
+           	name:"ali",
+           	age:20,
+           	class:"10th"
+           },
+           {
+           	name:"ahmad",
+           	age:20,
+           	class:"10th"
+           },
+           {
+           	name:"zain",
+           	age:20,
+           	class:"10th"
+           }
+           ];
+           console.log(typeof a);
